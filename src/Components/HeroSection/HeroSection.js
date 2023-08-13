5686082d60cb5fd6a167158c9edae6816c5d56d4
aabenezer <122594088+aabenezer@users.noreply.github.com>
2023-08-12 23:19:19 -0700

    Added new images for the hero section.

--- src/Components/HeroSection/HeroSection.js
+++ src/Components/HeroSection/HeroSection.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import vegaswide from '../../assets/img/las vegas wide 1.png';
+import vegaswide from '../../assets/img/las vegas wide 8.jpg';
 import './HeroSection.css';
+import { Link } from 'react-router-dom';
 import ab from '../../pages/HotelsCasinos';
+import HotelsCasinos from '../../pages/HotelsCasinos';
 
 const HeroSection = () => {
     return (
         <div className='hero-container'>
             <div>
                 <img src={vegaswide} alt='lasvegas-image' className='hero-image' />
+                <div className='hero-text'>
+                    <h2>The City that Never Sleeps</h2>
+                    <p>Las Vegas is a dazzling desert oasis that never sleeps,
+                        known for its vibrant nightlife, extravagant casinos with
+                        a larger-than-life reputation as the entertainment
+                        capital of the world.</p>
+                    <button className='hero-button'>
+                        <Link to='/HotelsCasinos' className='hero-button'>Explore</Link>
+                    </button>
             </div>
-            <div className='hero-text'>
-                <h2>The City that Never Sleeps</h2>
-                <p>Las Vegas is a dazzling desert oasis that never sleeps,
-                    known for its vibrant nightlife, extravagant casinos with
-                    a larger-than-life reputation as the entertainment
-                    capital of the world.</p>
-                <button className='hero-button' href={ab}>Explore</button>
+            
 
             </div>
 
